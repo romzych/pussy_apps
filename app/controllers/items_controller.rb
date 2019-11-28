16cff1880958cbbc2afa76495ea48caf64032e60
assouev <bsngr.asv@gmail.com>
2019-11-28 20:53:32 +0100

    views for seed ex: items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    @all_items = []
+    Item.all.each do |item|
+      @all_items << item
+    end
+    puts "#" * 60
+    puts @all_items
+    puts "#" * 60
   end
 
   def show
+    @item = Item.find(params[:id])
   end
-
-  def new
-  end
-
+  
   def create
-  end
-
-  def destroy
-  end
-
-  def edit
-  end
-
-  def update
   end
 end
