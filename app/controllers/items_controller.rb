9c62c427c2320bd9b72f2a4883b03cb16f42935a
Roman Blondel <rblondel23@gmail.com>
2019-11-29 02:02:13 +0100

    first feature

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,29 @@
 class ItemsController < ApplicationController
   def index
-    @all_items = []
+    @all_items      = []
+    @big_items      = []
+    @persan_items   = []
+    @siamois_items  = []
     Item.all.each do |item|
       @all_items << item
     end
+    Item.all.each do |item|
+      if item.race == "big"
+        @big_items << item
+      elsif item.race == "persan"
+        @persan_items << item
+      else
+        @siamois_items << item
+      end
+    end
+    puts "#" * 60
+    puts @big_items
     puts "#" * 60
     puts @all_items
+    puts "#" * 60
+    puts @siamois_items
+    puts "#" * 60
+    puts @persan_items
     puts "#" * 60
   end
 
